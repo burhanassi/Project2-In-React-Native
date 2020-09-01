24eeefc14dbfdea59007b89f1e50bb3d56a505a6
burhanassi <61707044+burhanassi@users.noreply.github.com>
2020-09-01 10:32:20 +0300

    Do some styling.

--- components/NumberAgreeForm.js
+++ components/NumberAgreeForm.js
@@ -11,6 +11,8 @@ const NumberAgreeForm = ({route, navigation}) => {
 
     const {num} = route.params;
 
+    const number = parseInt(JSON.stringify(num));
+
     const plusButtonHandler = () => {
         checkHandler();
         if(randNum > min){
@@ -32,27 +34,78 @@ const NumberAgreeForm = ({route, navigation}) => {
     };
 
     const checkHandler = () => {
-        if(randNum == JSON.stringify(num)){
-            navigation.navigate('Summary', {num: JSON.stringify(num)});
+        if(randNum === number){
+            navigation.navigate('Summary', {num: number});
         }
     }
 
     return (
-        <View>
-            <View >
-                <Text>
-                    <Text>This is a random number.</Text>
-                    <Text>Your number is less or more?</Text>
-                </Text>
-                <Text >
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 50
+        }}>
+            <View style={{
+                flex: 1,
+                width: 390,
+                flexDirection: 'column',
+                alignItems: 'center',
+                borderColor: 'black',
+                borderWidth: 1,
+                borderRadius:  40,
+                height: 200,
+                backgroundColor: '#fff'
+            }}>
+                <View style={{
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Text style={{
+                        fontWeight: 'bold',
+                        fontSize: 25,
+                        marginTop: 20,
+                        borderBottomWidth: 1.5,
+                        borderColor: 'gray',
+                    }}>This is a random number.</Text>
+                    <Text style={{
+                        fontWeight: 'bold',
+                        fontSize: 25,
+                        marginTop: 10,
+                        borderBottomWidth: 1.5,
+                        borderColor: 'gray',
+                    }}>Your number is less or more?</Text>
+                </View>
+                <Text style={{
+                    fontWeight: 'bold',
+                    fontSize: 30,
+                    marginTop: 40,
+                    borderColor: 'gray',
+                    borderWidth: 1,
+                    borderRadius: 50,
+                    padding: 20
+                }}>
                     {randNum}
                 </Text>
-                <View>
-                    <Button title={"+"} onPress={() => plusButtonHandler()}/>
-                    <Button title={"-"} onPress={() => minusButtonHandler()}/>
+                <View style={{
+                    width: 250,
+                    marginTop: 40,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                }}>
+                    <Button color={"red"} title={"More"} onPress={() => plusButtonHandler()}/>
+                    <Button color={"green"} title={"Less"} onPress={() => minusButtonHandler()}/>
                 </View>
+                <View style={{
+                    marginTop: 30,
+                    width: 700,
+                    paddingBottom: 30,
+                    borderBottomWidth: 1.5,
+                    borderColor: 'gray'
+                }}></View>
+                <NumbersList randNumList={randNumList}/>
             </View>
-            <NumbersList randNumList={randNumList}/>
         </View>
     );
 };
